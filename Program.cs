3a91a89daf197c8df4381ce70b3427a1f8946ee7
GuilhermeHPSilva <guilherme.pinheirohs@gmail.com>
2021-09-20 15:22:53 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ArrayColection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[5]{
+                "Dani",
+                "Maju",
+                "Bruno",
+                "Ramon",
+                "Sofi"
+            };
+            Console.WriteLine(nomes[2]);
+
+            //nomes[2] = "Antonio Junior";
+
+            Console.WriteLine("------");
+
+            Console.WriteLine($"Novo valor na index > {nomes[2]}");
+
+            Console.WriteLine("------ Iteração do Array pelo foreach");
+
+            foreach (string aluno in nomes)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            Console.WriteLine("------ Iteração do Array pelo for");
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Comandos do git
+1 - git init - (comando para inicializar o git)
+2 - git remote add origin https://github.com/GuilhermeHPSilva/Estrutura-Repeticao-Array-21008.git
+3 - git remote -v (comando para visualizar o repositório atual)
+4 - git commit -m "nome do commit" (o comando commit faz o pacote de alterações)
+5 - git push -u origin main
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
